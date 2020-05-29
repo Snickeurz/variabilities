917a952f79c973cae1ed35ff52e7990eb0a8ee81
Snickeurz <nicolassibaud@hotmail.com>
2020-05-29 10:35:27 +0200

    Update

--- code/src/main/java/choco/sudoku/Sudoku.java
+++ code/src/main/java/choco/sudoku/Sudoku.java
@@ -6,6 +6,9 @@ import org.chocosolver.solver.Solver;
 import org.chocosolver.solver.exception.ContradictionException;
 import org.chocosolver.solver.variables.IntVar;
 
+import java.util.Arrays;
+
+
 /**
  * Choco implementation of Sudoku.
  */
@@ -62,12 +65,43 @@ public class Sudoku
     };
 
     /**
+     * Grid with 25x25
+     */
+    static int[][] GOD_LIKE = {
+            {23,0,20,8,15,0,21,6,17,0,0,0,0,0,0,24,19,0,0,0,12,0,0,0,2},
+            {0,0,14,3,11,0,16,2,0,13,0,0,0,17,0,0,15,0,18,0,0,0,10,0,0},
+            {0,0,0,4,7,14,0,0,0,0,0,0,1,0,0,8,0,3,0,0,0,11,0,22,24},
+            {0,0,0,6,0,23,0,0,0,0,0,0,0,19,0,10,0,0,0,25,0,0,0,0,0},
+            {10,0,9,0,13,0,8,15,19,0,24,0,0,22,6,0,0,11,12,0,0,18,4,0,20},
+            {0,3,0,0,10,0,25,0,0,16,0,0,4,21,9,22,0,0,8,20,0,12,0,7,17},
+            {20,25,8,1,18,19,24,0,9,0,0,0,0,0,22,0,14,0,2,0,0,0,3,11,0},
+            {16,0,23,0,22,0,0,0,18,4,17,0,0,10,0,7,0,0,15,5,0,0,0,24,0},
+            {21,0,5,0,19,12,0,0,0,0,11,8,0,20,0,0,0,9,0,16,15,0,0,0,18},
+            {0,0,7,15,0,0,5,0,10,17,3,0,0,0,0,21,0,6,0,19,0,22,14,0,0},
+            {18,0,0,0,0,0,0,0,23,0,2,10,0,5,0,20,3,0,21,1,0,0,10,0,0},
+            {0,23,21,2,5,0,6,0,15,0,19,0,12,16,0,0,0,0,24,7,0,9,0,8,0},
+            {0,0,6,20,0,0,0,0,0,5,9,0,24,7,0,0,13,0,23,15,0,16,19,0,0},
+            {0,0,0,0,0,0,19,0,2,4,0,1,0,18,0,3,16,0,2,14,11,0,4,17,0,15},
+            {0,0,0,10,0,0,12,22,0,0,6,0,25,0,13,4,18,0,0,0,1,0,0,2,0},
+            {0,2,0,0,0,5,23,0,0,22,0,0,0,0,0,0,9,25,0,0,21,1,0,0,11},
+            {0,0,0,21,0,18,11,0,0,0,0,0,0,13,2,5,0,17,0,6,0,0,25,0,16},
+            {0,0,0,17,4,0,0,0,0,25,0,15,16,8,19,0,0,0,0,21,0,23,0,14,22},
+            {0,0,0,0,0,0,0,0,0,15,0,6,0,0,18,0,8,7,0,12,0,0,10,0,0},
+            {0,0,18,22,14,13,0,24,6,20,0,3,7,0,0,11,0,0,0,0,0,0,0,12,0},
+            {0,0,1,7,0,0,0,8,13,0,0,0,20,0,0,0,0,5,0,0,17,0,22,16,9},
+            {0,0,12,13,0,2,22,25,7,0,0,0,0,1,0,19,0,0,0,14,0,3,20,18,23},
+            {15,10,0,0,6,3,14,4,0,0,0,0,23,0,0,12,0,0,11,18,5,2,0,19,0},
+            {0,19,4,0,3,17,9,0,11,0,22,0,0,2,15,0,0,0,0,0,0,14,6,0,1},
+            {14,0,0,24,23,1,0,0,0,0,0,0,6,0,0,9,25,22,0,0,4,0,0,0,12},
+    };
+
+    /**
      * Main method.
      *
      * @param args jvm args
      * @throws ContradictionException in case of propagation fail
      */
-    public static void main(final String[] args) throws ContradictionException
+    public static void main(final String[] args)
     {
         long startTime = System.nanoTime();
         // *** DECLARE THE MAIN ATTRIBUTES ***
@@ -83,9 +117,14 @@ public class Sudoku
         int n2 = n * n;
 
         // choose grid
-        // int[][] mySudokuGrid = GRID_HARD;
+        // 9x9
+         int[][] mySudokuGrid = GRID_HARD;
+//         int[][] mySudokuGrid = GRID_DIABOLIK;
+
+        // 16x16
         // int[][] mySudokuGrid = MONSTER_GRID;
-        int[][] mySudokuGrid = GRID_DIABOLIK;
+        // 25x25
+        // int[][] mySudokuGrid = GOD_LIKE;
 
         // Set boundaries values
         // We want values from 1 to 9 (0 is for blank case, i.e find a value to fit in)
@@ -114,7 +153,7 @@ public class Sudoku
                 // in case of blank case (i.e, 0)
                 if (mySudokuGrid[i][j] == 0)
                 {
-                    // this cell will contain value between lowerbound (1) to upperboud (9)
+                    // contain value between lowerbound (1) and upperboud (9)
                     t[i][j] = model.intVar(lowerBound, upperBound);
                 }
                 else
@@ -135,6 +174,10 @@ public class Sudoku
         {
             // Get column array
             System.arraycopy(t[i], 0, columns[i], 0, n2);
+            for (int j = 0; j < n2 ; j++)
+            {
+                columns[i][j] = t[i][j];
+            }
 
             // Get line array
             lines[i] = t[i];
@@ -143,10 +186,10 @@ public class Sudoku
         // *** SET CONSTRAINTS ON REGIONS ***
         // regions can be numbered (r) from 0 to n2 (excluded)
         // Given my region
-        IntVar[][] r = new IntVar[n2][n2];
+        IntVar[][] regions = new IntVar[n2][];
 
         // for a region r you can do 2 nested loops on i and j
-        for (int x = 0; x < n2; x++)
+        for (int region = 0; region < n2; region++)
         {
             IntVar[] block = new IntVar[n2];
 
@@ -154,20 +197,20 @@ public class Sudoku
             int counter = 0;
 
             // region r: first coord i0 = r / n * n
-            int i0 = x / n * n;
+            int i0 = region / n * n;
             // and j0 = r % n * n
-            int j0 = x / n * n;
+            int j0 = region % n * n;
 
             // i from i0 to i0+n (excluded)
-            for (int i = i0; i < i0 + n ; i++)
+            for (int i = i0; i < (i0 + n) ; i++)
             {
                 // and j from j0 to j0+n (excluded)
-                for (int j = j0; j < j0 + n; j++)
+                for (int j = j0; j < (j0 + n); j++)
                 {
                     block[counter] = t[i][j];
                     counter++;
                 }
-                r[x] = block;
+                regions[region] = block;
             }
         }
 
@@ -176,15 +219,26 @@ public class Sudoku
         {
             model.allDifferent(lines[i]).post();
             model.allDifferent(columns[i]).post();
-            model.allDifferent(r[i]).post();
+            model.allDifferent(regions[i]).post();
         }
 
         // Tell to solver to propagate constraints in order to reach endpoint
-        solver.propagate();
+        try
+        {
+            solver.propagate();
+        }
+        catch (ContradictionException ce)
+        {
+            System.out.println("Le problème n'a pas de solution !");
+            System.exit(1);
+        }
 
         // *** SOLVE ***
         // find solution
         Solution solution = solver.findSolution();
+
+        System.out.println(solver.solve());
+
         // if solution is null, then we are doomed
         if (solution == null)
         {
@@ -196,8 +250,14 @@ public class Sudoku
             // Call helper method to display sudoku
             displaySudoku(n, t);
 
+            solver.showShortStatisticsOnShutdown();
+
+            // Add end time
             long endTime = System.nanoTime();
+            // calcul duration
             long duration = (endTime - startTime);
+
+            // print out
             System.out.println("");
             System.out.println("Solution found in : " + duration / 1000000 + " ms");
         }
